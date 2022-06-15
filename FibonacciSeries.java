5784d886da7f1107d84e6c11fdfc36f92f62d541
IndhuKamal <107314957+IndhuKamal@users.noreply.github.com>
2022-06-15 18:42:39 +0800

    Add files via upload

---
+++ FibonacciSeries.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class FibonacciSeries {
+public static void main(String[] args) {
+	int f=0,s=1;
+	
+	for(int i=0;i<=10;i++) {
+		System.out.println(f);
+		{
+		int t=f+s;
+		f=s;
+		s=t;
+		}
+	}
+	
+}
+		
+}
