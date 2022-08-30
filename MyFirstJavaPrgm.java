81aed2ec0b4bfeb9544e10a6a4dac3f1b8d4d288
IndhuKamal <107314957+IndhuKamal@users.noreply.github.com>
2022-08-30 15:55:19 +0800

    Add files via upload

---
+++ MyFirstJavaPrgm.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class MyFirstJavaPrgm {
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		
+		System.out.println("Welcome to Testleaf");
+		
+	float a=5.5f;
+		int b=7;
+		float c=a+b;
+		System.out.println(a+b);
+		System.out.println("c values "+c);
+				
+	}
+
+}
